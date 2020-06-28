a71c1f06c7dbff2d012c391387358431b56cb944
quyvsquy <17520960@gm.uit.edu.vn>
2020-06-28 17:47:08 +0700

    fix

--- auto_deploy_colab.sh
+++
@@ -1,9 +0,0 @@
-# scp -P 18376 auto_deploy_colab.sh  root@2.tcp.ngrok.io:/root
-
-apt install vim -y
-git clone https://github.com/quyvsquy/demoUnet_Segnet_FCN_PSPnet_
-cd demoUnet_Segnet_FCN_PSPnet_
-wget https://media.githubusercontent.com/media/quyvsquy/demoUnet_Segnet_FCN_PSPnet_/master/dataset1.zip
-unzip dataset1.zip.1
-mkdir saveModel
-vi test.py
