dda58f0dac1474ae09969ddd9e407b78e05b361a
quyvsquy <17520960@gm.uit.edu.vn>
2020-06-28 15:18:35 +0700

    at file

---
+++ auto_deploy_colab.sh
@@ -0,0 +1,7 @@
+apt install vim -y
+git clone https://github.com/quyvsquy/demoUnet_Segnet_FCN_PSPnet_
+cd demoUnet_Segnet_FCN_PSPnet_
+wget https://media.githubusercontent.com/media/quyvsquy/demoUnet_Segnet_FCN_PSPnet_/master/dataset1.zip
+unzip dataset1.zip.1
+mkdir saveModel
+vi test.py
