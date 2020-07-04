f40e0fc7c3f5bc3aa7183c67dc493c135cd172de
quyvsquy <17520960@gm.uit.edu.vn>
2020-07-04 21:24:56 +0700

    fix

--- testLoadModel.py
+++ testLoadModel.py
@@ -1,13 +1,25 @@
 from keras_segmentation.predict import predict_multiple
+from keras_segmentation.predict import evaluate
 checkpoints_path = []
 checkpoints_path.append("./saveModel/vgg_unet_1")
 checkpoints_path.append("./saveModel/vgg_pspnet_1")
 checkpoints_path.append("./saveModel/vgg_segnet_1")
 checkpoints_path.append("./saveModel/fcn_32_vgg_1")
 
+# for ia in checkpoints_path:
+# 	predict_multiple(
+# 		checkpoints_path=ia,
+# 		inp_dir="demo/images_preped_test_small",
+# 		out_dir="demo/output"
+# 	)
+
+
+
+
 for ia in checkpoints_path:
-	predict_multiple(
+	output = evaluate(
 		checkpoints_path=ia,
-		inp_dir="demo/images_preped_test_small",
-		out_dir="demo/output"
+		inp_images_dir="dataset1/images_prepped_test/",
+		annotations_dir = "dataset1/annotations_prepped_test/",
 	)
+	print(output)
