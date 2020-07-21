7919556145c236e93a405054b0f296cb91f83cbd
quyvsquy <17520960@gm.uit.edu.vn>
2020-07-22 00:28:35 +0700

    fix

--- train_small_all.py
+++ train_small_all.py
@@ -1,25 +1,19 @@
 from keras_segmentation.models.all_models import model_from_name
+import pickle
 
 #python3 -m keras_segmentation visualize_dataset --images_path="./demo/images_preped_test_small/" --segs_path="./demo/annotations_preped_test_small/" --n_classes=30
-model_name = []
-model = []
-model_name.append("vgg_unet")
-model_name.append("vgg_pspnet")
-model_name.append("vgg_segnet")
-model_name.append("fcn_32_vgg")
-checkpoints_path = []
-checkpoints_path.append("./saveModel/vgg_unet_1")
-checkpoints_path.append("./saveModel/new1/vgg_pspnet_1")
-checkpoints_path.append("./saveModel/vgg_segnet_1")
-checkpoints_path.append("./saveModel/fcn_32_vgg_1")
+dictModels = {}
+dictModels[0] = "vgg_unet"
+dictModels[1] = "vgg_pspnet"
+dictModels[2] = "vgg_segnet"
+dictModels[3] = "fcn_32_vgg"
 
-for ia in model_name:
-    model.append(model_from_name[ia](n_classes=6 ,  input_height=384, input_width=576))
-
-for ib,ia in enumerate(model):
-    ia.train(
-        batch_size = 3,
+for ia in len(dictModels):
+    t = model_from_name[dictModels[ia]](n_classes=12 ,  input_height=384, input_width=576)
+    H = t.train(
+        verify_dataset = False,
         epochs= 50,
+        batch_size = 3,
         steps_per_epoch = 337 // 3,
         train_images =  "dataset1/train",
         train_annotations = "dataset1/ano_train/",
@@ -29,7 +23,7 @@ for ib,ia in enumerate(model):
         val_batch_size = 1,
         val_steps_per_epoch = 30 // 1,
         # gen_use_multiprocessing = True,
-        checkpoints_path = checkpoints_path[ib] 
+        checkpoints_path = f"./saveModel/{dictModels[ia]}_1" 
     )
-
+    pickle.dump(H.history,open(f"./saveModel/His/{dictModels[ia]}","wb"))
 
