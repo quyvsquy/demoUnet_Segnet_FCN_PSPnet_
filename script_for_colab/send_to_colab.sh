63407e4a2dbb23b51e3be80d4d3094ee0a4501cb
quyvsquy <17520960@gm.uit.edu.vn>
2020-07-22 23:45:34 +0700

    done

--- script_for_colab/send_to_colab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# scp -P 10927 root@0.tcp.ngrok.io:/root/demoUnet_Segnet_FCN_PSPnet_/saveModel/His/unet_0 ./His
-PORT=$1
-IP=$2
-sshpass -p 123456 scp -o StrictHostKeyChecking=no -P $PORT auto_deploy_colab.sh  root@$IP.tcp.ngrok.io:"/content/gdrive/My\ Drive/HKVI/CS431_CacKyThuatHocSauVaUngDung"
-sshpass -p 123456 ssh -o StrictHostKeyChecking=no root@$IP.tcp.ngrok.io -p $PORT
